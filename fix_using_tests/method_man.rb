04f4a71e9f14c9fa0c504be262db443df68971f1
Jake Chappelle <jake.m.chappelle@gmail.com>
2020-08-16 14:08:36 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -1,12 +1,9 @@
 # don't forget to add: require 'pry'
-require 'pry'
 
 def start_game(player1, player2)
   "Hello #{player1} & #{player2}"
-  binding.pry
 end
 
 def play_game(player1, player2)
   "#{player1} is better than #{player2}"
-  binding.pry
 end
