9e075a3af4c7f1b390bc2bfea738910c5815b0b4
Jake Chappelle <jake.m.chappelle@gmail.com>
2020-08-16 13:46:09 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -5,16 +5,11 @@ require 'pry'
 
 
 def start_game(player1, player2)
-  "Hello #{player1} & #{playar2}"
+  "Hello #{player1} & #{player2}"
   binding.pry
 end
 
-def play_game(player2)
+def play_game(player1, player2)
   "#{player1} is better than #{player2}"
   binding.pry
 end
-
-player1 = "Stephen"
-player2 = "David"
-start_game(player1, player2)
-play_game(player1, player2)
